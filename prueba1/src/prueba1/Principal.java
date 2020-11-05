50d745a232f22547fe6706bf6d854138f814d4ee
MarcosPC <MarcosPC@DESKTOP-TU8COEC>
2020-11-05 18:13:09 +0100

    Cambios. Paso 2

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -5,8 +5,10 @@ public class Principal
 
     public static void main(String[] args)
     {
-        // TODO Auto-generated method stub
-
+        System.out.println("Hola mundo");
+        System.out.println("Hola mundo2");
+        System.out.println("Hola mundo3");
+        
     }
 
 }
